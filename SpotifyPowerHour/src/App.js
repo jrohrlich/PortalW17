ac493782faf5ec4578659722a955ce02cb18429e
kane thomas <kanethomas@kanes-MacBook-Pro.local>
2016-12-17 09:19:08 -0500

    updated app.js

--- SpotifyPowerHour/src/App.js
+++ SpotifyPowerHour/src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Welcome to React<code>src/App.js</code> and save to reload.
           <br />
           <br />
           <br />
